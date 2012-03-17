e918a4bd36451c3adc0e133e7ebc90c91eabe72b
A. Samuel Pottinger <Samnsparky@gmail.com>
2012-03-17 13:16:05 -0600

    Changed file extension for arduino compliance

--- aquariumlogic/aquariumlogic.ino
+++ aquariumlogic/aquariumlogic.ino
@@ -17,7 +17,6 @@ Aquarium aquariums[NUM_AQUARIUMS];
 void setup()
 {
   Serial.begin(9600);
-  randomSeed(analogRead(RAND_AIN_PORT));
   crs_init(0, 9, 0, true);
 }
 
@@ -36,7 +35,7 @@ void crs_init(int id, byte controlLine, byte potLine, boolean zero)
 
   target->controlLine = controlLine;
   target->potLine = potLine;
-  target->zeroValue = PRE_CALIBRATION_ZERO_VAL;
+  target->zeroValue = PRE_CALIBRATION_ZERO_VALUE;
   target->position = PRE_CALIBRATION_POSITION;
   target->targetPosition = PRE_CALIBRATION_POSITION;
   target->targetVel = STARTING_TARGET_VELOCITY;
@@ -283,14 +282,14 @@ void crs_calibrate_(int id)
   target->targetPosition = 0;
 
   // Try another velocity for x2 and y2
-  crs_setVelocity_(id, START_CALIBRATION_VEL);
+  crs_setVelocity(id, START_CALIBRATION_VEL);
   delay(100);
 
   // Calculate slope
   target->targetVel = ((float)(y2-y1))/(x2-x1);
 
   // Go back
-  crs_setVelocity_(id, -START_CALIBRATION_VEL);
+  crs_setVelocity(id, -START_CALIBRATION_VEL);
   delay(100);
 
   // Go back to zero
@@ -598,41 +597,34 @@ void fish_goToNextInternalGoal_(int id)
 
 void fish_step(int id, long ms)
 {
-  Fish * target = fish_getInstance(id);
-  crs_step(target->xServo, ms);
-  crs_step(target->yServo, ms);
-  crs_step(target->zServo, ms);
-  crs_step(target->thetaServo, ms);
+    Fish * target = fish_getInstance(id);
+    crs_step(target->xServo, ms);
+    crs_step(target->yServo, ms);
+    crs_step(target->zServo, ms);
+    crs_step(target->thetaServo, ms);
 }
 
-void fish_setVelocity(int id, float velocity)
-{
-  Fish * target = fish_getInstance(id);
-  target->velocity = velocity;
-}
-
-PiezoSensorGroup * psg_getInstance(int id)
+PiezoSensorGroup * pse_getInstance(int id)
 {
   return &(piezoSensorGroups[id]);
 }
 
-void psg_init(int id, int numSensors)
+void pse_init(int id, int numSensors);
 {
-  PiezoSensorGroup * target = psg_getInstance(id);
+  PiezoSensorGroup * target = pse_getInstance(id);
   target->numSensors = numSensors;
   target->nextElementIndex = 0;
-  target->sensorNums = (SensorGroupMembershipRecord *)(malloc(numSensors * sizeof(SensorGroupMembershipRecord)));
+  target->sensorsNums = malloc(numSensors * sizeof(SensorGroupMembershipRecord));
 }
 
-void psg_dest(int id)
+void pse_dest(int id);
 {
-  PiezoSensorGroup * target = psg_getInstance(id);
   free(target->sensorNums);
 }
 
-void psg_addToSensorList(int id, int sensorID, int sensorHighLevelID)
+void pse_addToSensorList(int id, int sensorID, int sensorHighLevelID){
 {
-  PiezoSensorGroup * target = psg_getInstance(id);
+  PiezoSensorGroup * target = pse_getInstance(id);
   int nextElementIndex = target->nextElementIndex;
 
   SensorGroupMembershipRecord * record = &(target->sensorNums[nextElementIndex]);
@@ -642,21 +634,21 @@ void psg_addToSensorList(int id, int sensorID, int sensorHighLevelID)
   nextElementIndex++;
 }
 
-int psg_getTapped(int id)
+void pse_getTapped(int id)
 {
   int i;
   int sensorID;
   int sensorVal;
   int maxSensorRecordIndex;
   int maxSensorVal;
-  PiezoSensorGroup * target = psg_getInstance(id);
+  PiezoSensorGroup * target = pse_getInstance(id);
     
   maxSensorRecordIndex = NONE;
   maxSensorVal = 0;
   for(i = 0; i < target->nextElementIndex; i++)
   {
     sensorID = target->sensorNums[i].sensorID;
-    sensorVal = piezo_isFired(sensorID);
+    sensorVal = peizo_isFired(sensorID);
     if(sensorVal > 0 && sensorVal > maxSensorVal)
     {
       maxSensorVal = sensorVal;
@@ -665,7 +657,7 @@ int psg_getTapped(int id)
   }
 
   if(maxSensorRecordIndex != NONE)
-    return NONE;
+    return NONE
   else
     return target->sensorNums[maxSensorRecordIndex].highLevelID;
 }
@@ -675,8 +667,8 @@ Aquarium * aquarium_getInstance(int id)
   return &(aquariums[id]);
 }
 
-void aquarium_init(int id, int fishNum, int jellyfishNum, int lightSensorNum,
-                   int piezoSensorGroupNum)
+void aquarium_init(int id, int fishNum, int jellyfishNum, int lightSensorNum
+                   int piezeoSensorGroupNum)
 {
   Aquarium * target = aquarium_getInstance(id);
     
@@ -704,7 +696,7 @@ void aquarium_shortStep(int id, long ms)
   target = aquarium_getInstance(id);
     
   // Check sensors
-  tappedSensor = psg_getTapped(target->piezoSensorGroupNum);
+  tappedSensor = pse_getTapped(target->piezoSensorGroupNum);
   curLight = ls_isLight(target->lightSensorNum);
 
   // Respond to tap
@@ -712,8 +704,10 @@ void aquarium_shortStep(int id, long ms)
       aquarium_runFishToOpposingSide_(id, tappedSensor);
 
   // Repond to light
-  if(curLight != target->isLight) // If the light sensor state has changed
+  if(curLight != aquarium->isLight) // If the light sensor state has changed
   {
+    aquarium->isLight = curLight;
+    
     if(curLight)
       aquarium_transitionToFishState_(id);
     else
@@ -762,140 +756,5 @@ void aquarium_tick(int id, long newMS)
 
 void aquarium_onFishReachedGoal(int id, int fishID)
 {
-  int xRand;
-  int yRand;
-  int zRand;
-  int randSpeed;
-  int targetFish;
-  Aquarium * target;
   
-  target = aquarium_getInstance(id);
-  targetFish = target->fishNum;
-  
-  if(target->isLight)
-  {
-    randSpeed = (int)(random(MIN_FISH_SPEED, MAX_FISH_SPEED));
-    fish_setVelocity(targetFish, randSpeed);
-
-    xRand = (int)(random(MIN_X_VAL, MAX_X_VAL));
-    yRand = (int)(random(MIN_Y_VAL, MAX_Y_VAL));
-    zRand = (int)(random(MIN_Z_VAL, MAX_Z_VAL));
-
-    fish_goTo(targetFish, xRand, yRand, zRand);
-  }
-}
-
-void aquarium_runFishToOpposingSide_(int id, int tappedSensor)
-{
-  int opposingDirection;
-  int targetX;
-  int targetY;
-  int targetZ;
-  int fishNum;
-  Aquarium * target;
-
-  // Get required structures
-  target = aquarium_getInstance(id);
-  fishNum = target->fishNum;
-
-  // Determine direction and corresponding corrdinates
-  opposingDirection = aquarium_getOpposingDirection_(id, tappedSensor);
-  targetX = aquarium_getXBoundInDirection_(id, opposingDirection);
-  targetY = aquarium_getYBoundInDirection_(id, opposingDirection);
-  targetZ = MIN_Z_VAL;
-
-  // Update fish
-  fish_setVelocity(fishNum, MAX_FISH_SPEED);
-  fish_goTo(fishNum, targetX, targetY, targetZ);
-}
-
-void aquarium_transitionToFishState_(int id)
-{
-    Aquarium * target;
-    int jellyfishNum;
-    int fishNum;
-    
-    // Get access to required structes
-    target = aquarium_getInstance(id);
-    jellyfishNum = target->jellyfishNum;
-    fishNum = target->fishNum;
-    
-    // Update internal state to let fish move
-    target->isLight = true;
-
-    // Raise jellyfish
-    jellyfish_raise(jellyfishNum);
-
-    // Start fish a moving
-    fish_onGoalReached(fishNum);
-}
-
-void aquarium_transitionToJellyfishState_(int id)
-{
-    Aquarium * target;
-    int jellyfishNum;
-    int fishNum;
-    
-    // Get access to required structes
-    target = aquarium_getInstance(id);
-    jellyfishNum = target->jellyfishNum;
-    fishNum = target->fishNum;
-
-    // Lower jellyfish
-    jellyfish_lower(jellyfishNum);
-
-    // Hide the fish
-    fish_goTo(fishNum, CENTRAL_X_VAL, CENTRAL_Y_VAL, MIN_Z_VAL);
-
-    // Update internal state to stop fish
-    target->isLight = false;
-}
-
-int aquarium_getXBoundInDirection_(int id, int direction)
-{
-    switch(direction)
-    {
-        case NORTH:
-        case NORTHWEST:
-        case WEST:
-        case SOUTHWEST:
-            return MIN_X_VAL;
-        case NORTHEAST:
-        case EAST:
-        case SOUTHEAST:
-        case SOUTH:
-            return MAX_X_VAL;
-    }
-}
-
-int aquarium_getYBoundInDirection_(int id, int dir)
-{
-    switch(dir)
-    {
-        case WEST:
-        case NORTHWEST:
-        case NORTH:
-        case NORTHEAST:
-            return MAX_Y_VAL;
-        case EAST:
-        case SOUTHEAST:
-        case SOUTH:
-        case SOUTHWEST:
-            return MIN_Y_VAL;
-    }
-}
-
-int aquarium_getOpposingDirection_(int id, int dir)
-{
-  switch(dir)
-  {
-    case NORTH: return SOUTH;
-    case NORTHEAST: return SOUTHWEST;
-    case EAST: return WEST;
-    case SOUTHEAST: return NORTHWEST;
-    case SOUTH: return NORTH;
-    case SOUTHWEST: return NORTHEAST;
-    case WEST: return EAST;
-    case NORTHWEST: return SOUTHEAST;
-  }
 }
